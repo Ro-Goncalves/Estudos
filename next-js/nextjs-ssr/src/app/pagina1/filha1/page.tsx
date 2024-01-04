5241425dd374beeb129bd0fa55ddcdab41494bd9
ro.go.calves <ro.go.calves@gmail.com>
2024-01-03 22:12:01 -0300

    nextjs - Full Cycle - nextjs-ssr

---
+++ next-js/nextjs-ssr/src/app/pagina1/filha1/page.tsx
@@ -0,0 +1,9 @@
+function Filha1Page(){
+    return(
+        <div>
+            <h1>Filha 1</h1>
+        </div>
+    );
+}
+
+export default Filha1Page;
---
+++ next-js/nextjs-ssr/src/app/pagina1/page.tsx
@@ -0,0 +1,9 @@
+function Pagina1Page(){
+    return(
+        <div>
+            <h1>Página 1</h1>
+        </div>
+    );
+}
+
+export default Pagina1Page;
