fd0f02cd664f62eeb0edfc52f89362eb0c912c8a
ro.go.calves <ro.go.calves@gmail.com>
2024-02-27 15:12:02 -0300

    Automatizando Testes Java - Estudo - Arrumando a tag @SpringBootTest

--- testes/alura-automatizando-testes-java/src/test/java/br/com/alura/adopet/AdopetApiApplicationTests.java
+++ testes/alura-automatizando-testes-java/src/test/java/br/com/alura/adopet/AdopetApiApplicationTests.java
@@ -3,7 +3,9 @@ package br.com.alura.adopet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import br.com.alura.adopet.api.AdopetApiApplication;
+
+@SpringBootTest(classes = {AdopetApiApplication.class})
 class AdopetApiApplicationTests {
 
 	@Test
